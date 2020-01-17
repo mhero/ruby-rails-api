947baa4260ce6db54bc464a775de13b41df5b783
mhero <gdmarav374@gmail.com>
2020-01-17 10:45:37 -0500

    Add gem annotate to show full data

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,2 +1,15 @@
 class Medium < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: media
+#
+#  id         :integer          not null, primary key
+#  poster     :string
+#  title      :string
+#  year       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  imdb_id    :string
+#
