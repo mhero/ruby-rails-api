d3f697c43e99f0f9ad73e7dd1372d8162e88bf02
mhero <gdmarav374@gmail.com>
2020-01-18 05:51:00 -0500

    Call search method using controller params
    Add permitted params to controller

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,5 +1,13 @@
 class MediaController < ApplicationController
   def index
-    
+    result = Imdb::Client.new.search_by_title(
+      media_params[:title],
+      media_params
+    )
+    render json: result.handle(Medium)
+  end
+
+  def media_params
+    params.permit(:title, :year)
   end
 end
--- app/services/imdb/client.rb
+++ app/services/imdb/client.rb
@@ -1,11 +1,14 @@
 module Imdb  
   class Client
+    PLOT = "full"
+    FORMAT = "json"
+
     def initialize
       @connetion = Imdb::Http.new
     end
     
-    def search_by
-      result = @connetion.fetch("/", s: "love", plot: "full", r: "json")
+    def search_by_title(title, options = {})
+      result = @connetion.fetch("/", s: title, y: options[:year], plot: PLOT, r: FORMAT)
     end
   end
 end
