8aa629c1ff7dd226bfd8ab5096af1fc80a8ddb00
mhero <gdmarav374@gmail.com>
2020-01-18 12:39:45 -0500

    Add test for show action

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
-
 RSpec.describe MediaController do
+
     describe "index" do
       context "when searching by title" do
         it "returns 200 with valid params" do
@@ -26,7 +26,6 @@ RSpec.describe MediaController do
           expect(response.status).to eq 404
         end
       end
-
       context "when searching by year" do
         it "returns 200 with valid params" do
           VCR.use_cassette(
@@ -39,7 +38,6 @@ RSpec.describe MediaController do
     
           expect(response.status).to eq 200
         end
-
         it "returns 404 with invalid params" do
           VCR.use_cassette(
               "imdb/movie_year_error",
@@ -52,5 +50,29 @@ RSpec.describe MediaController do
         end
       end
     end
-  end
 
+    describe "#show" do
+      it "returns a successful response with a valid id" do
+        VCR.use_cassette(
+              "imdb/show_movie",
+              match_requests_on: [:body],
+              re_record_interval: 30.days
+            ) do
+              get :show, params: { id: "tt9561862" }
+        end
+        
+        expect(response.status).to eq 200
+      end
+      it "returns a 404 response with an invalid id" do
+        VCR.use_cassette(
+              "imdb/show_movie_error",
+              match_requests_on: [:body],
+              re_record_interval: 30.days
+            ) do
+              get :show, params: { id: "holamundo" }
+        end
+        
+        expect(response.status).to eq 404
+      end
+    end
+  end
