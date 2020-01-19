86b2a8761ad4c8839681626d70eefb3adad85b4f
mhero <gdmarav374@gmail.com>
2020-01-19 07:50:00 -0500

    Create react search for media
    - Using axios
    - Rendering results search
    - First element will be selected

--- omdb-react/src/app/App.js
+++ omdb-react/src/app/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+const axios = require('axios').default;
 
 const PosterCard = ({title, year, poster, selected}) => {
   return (
@@ -19,75 +20,63 @@ const MediumDetail = ({mediumDetails}) => {
 }
 
 const App = () => {
-  const [movies, setMovies] = useState(null);
+  const [media, setMedia] = useState(null);
+  const [search, setSearch] = useState(null);
+  const [error, setError] = useState(null);
+  const [selected, setSelected] = useState(0);
+
+  const updateError = (error) => {
+    if (error.response != null && error.response.status === 404) {
+      setError({status: 404, message: "No media found"});
+    } else {
+      setError({status: 500, message: "Error in server, please try again in some minutes"});
+    }
+  }
+
+  const getMedia = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/media?title=${search}`)
+    .then((response) => {
+      setMedia(response.data);
+    })
+    .catch((error) => {
+      updateError(error);
+    });
+  }
 
   return (
     <div className="App">
       <div className="search">
-        <input type="text" className= "search-input" id="name" name="name" placeholder="Search a Movie or Series title"/>
-        <button type="button">Search!</button>
+        <input type="text" className= "search-input" id="name" name="name" 
+               placeholder="Search a Movie or Series title"
+               onChange={(event) => setSearch(event.target.value) }/>
+        <button type="button" onClick={getMedia}>Search!</button>
       </div>
-      <div className="results">
-        <div className="movies-panel">
-          <ul>
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-              selected={true}
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-            <PosterCard 
-              title="hola Club" 
-              year="1999" 
-              poster="https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
-            />
-          </ul>
+
+      {media && 
+        <div className="results">
+          <div className="movies-panel">
+            <ul>
+              {media.map((item, index) => {
+                const {title, year, poster} = item;
+                return <PosterCard 
+                  title={title}
+                  year={year}
+                  poster={poster}                
+                  selected={index == selected}
+                />
+              })}
+            </ul>
+          </div>
+          <div className="details-panel">
+            <MediumDetail />
+          </div>
         </div>
-        <div className="details-panel">
-          <MediumDetail />
-        </div>
-      </div>
-          
+      }
+      {error && 
+        <div className="results movies-panel">
+          {error.message}
+        </div> 
+      }
     </div>
   );
 }
