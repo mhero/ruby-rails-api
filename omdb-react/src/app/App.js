c2bdfd0910eb9bc88029a7a337c2695289509e1e
mhero <gdmarav374@gmail.com>
2020-01-19 13:06:04 -0500

    Update details structure and css

--- omdb-react/src/app/App.js
+++ omdb-react/src/app/App.js
@@ -18,7 +18,37 @@ const PosterCard = ({item, onClick}) => {
 const MediumDetail = ({details}) => {
   const {title, runtime, released, actors, plot, imdb_rating} = details;
   return(
-    <div>{title}</div>
+    <>
+      <h1 className="medium-details title">{title}</h1>
+      <p>{plot}</p>
+      <table>
+      <thead>
+        <tr>
+            <th colspan="2">Stats</th>
+        </tr>
+    </thead>
+        <tr>
+          <th>IMDB Rating</th>
+          <td>{imdb_rating}</td>
+        </tr>
+        <tr>
+          <th>Date Released</th>
+          <td>{released}</td>
+        </tr>
+        <tr>
+          <th>Runtime</th>
+          <td>{runtime}</td>
+        </tr>
+      </table>
+      <h2>Actors:</h2>
+      <ul className="medium-details actors-list">{
+        actors.map(
+          (actor) => {
+            return <li>{actor}</li>
+          }
+        )}
+      </ul>
+    </>
   );
 }
 
@@ -100,8 +130,8 @@ const App = () => {
         </div>
       }
       {error && 
-        <div className="results movies-panel">
-          {error.message}
+        <div className="results error-panel">
+          <p>{error.message}</p>
         </div> 
       }
     </div>
